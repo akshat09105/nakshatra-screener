0e2a3c0e4ea6c372eed420afbc43d118f2d1854d
unknown <akshat9105@gmail.com>
2026-07-19 09:09:09 +0530

    changes

--- database/db.py
+++ database/db.py
@@ -2,17 +2,19 @@ import streamlit as st
 import psycopg2
 import pandas as pd
 
-
 def get_connection():
-    return psycopg2.connect(
-        host=st.secrets["DB_HOST"],
-        port=st.secrets["DB_PORT"],
-        database=st.secrets["DB_NAME"],
-        user=st.secrets["DB_USER"],
-        password=st.secrets["DB_PASSWORD"],
-        sslmode="require"
-    )
-
+    try:
+        return psycopg2.connect(
+            host=st.secrets["DB_HOST"],
+            port=st.secrets["DB_PORT"],
+            database=st.secrets["DB_NAME"],
+            user=st.secrets["DB_USER"],
+            password=st.secrets["DB_PASSWORD"],
+            sslmode="require",
+        )
+    except Exception as e:
+        st.error(str(e))
+        raise
 
 def get_companies():
     conn = get_connection()
